84d03a35ff899edcdc706fef5121ff09fdba2bc1
CodyPerakslis <CodyPerakslis@noreply.github.com>
2019-07-17 16:48:31 -0500

    convert http.Header to map[string][]string

--- dynamux.go
+++ dynamux.go
@@ -10,7 +10,7 @@ type DynamicRequest func(*http.Request) interface{}
 type DynamicResponse func(http.ResponseWriter)
 type Method func(data interface{}) DynamicResponse
 
-func New(header http.Header, req DynamicRequest, method Method) Handler {
+func New(header map[string][]string, req DynamicRequest, method Method) Handler {
   return func(w http.ResponseWriter, r *http.Request) {
     for k, v := range header {for _, elem := range v {w.Header().Set(k, elem)}}
     method(req(r))(w)
